1f620bcb84b887202e52f3c344aa06881eb9bacb
Niklas Polke <niklas.polke@gmx.de>
2016-03-25 15:11:08 +0100

    Set hsqldb to defrag and minimize db on checkpoint

--- src/main/java/vu/de/npolke/myexpenses/services/SystemDAO.java
+++ src/main/java/vu/de/npolke/myexpenses/services/SystemDAO.java
@@ -23,7 +23,7 @@ import java.sql.SQLException;
  */
 public class SystemDAO extends AbstractConnectionDAO {
 
-	private static final String SQL_CHECKPOINT = "CHECKPOINT";
+	private static final String SQL_CHECKPOINT = "CHECKPOINT DEFRAG";
 
 	public SystemDAO() {
 	}
